659bfab59833ada57c5e70a776ce7e390c3af707
anass91 <95028813+anass91@users.noreply.github.com>
2021-12-30 09:01:33 +0100

    Add files via upload

---
+++ code.js
@@ -0,0 +1,33 @@
+const inpuText = document.querySelector('.text');
+const demo = document.querySelector('.demo');
+
+
+    function getDataCountr(enterName){
+        const request = new XMLHttpRequest();
+        request.open('GET', `https://restcountries.com/v2/name/${enterName}`);
+        request.send();
+
+        request.addEventListener('load', function(){
+            const [data] = JSON.parse(this.responseText);
+            console.log(data);
+
+            const html = `
+            <article class="country">
+                <img class="country__img" src="${data.flag}"/>
+                <div class="country__data">
+                    <h3 class="country__name">${data.name}</h3>
+                    <h4 class="country__region">${data.region}</h4>
+                    <p class="country__row"><ion-icon class='icon1' name="analytics-outline"></ion-icon>${(+data.population / 1000000).toFixed(1)}</p>
+                    <p class="country__row"><ion-icon class="icon2" name="language-outline"></ion-icon>
+                    ${data.languages[0].name}</p>
+                    <p class="country__row"><ion-icon class="icon3" name="cash-outline"></ion-icon>${data.currencies[0].name}</p>
+                </div>
+            </article>
+                `;
+                demo.insertAdjacentHTML('beforeend', html)
+        });
+    }
+    document.querySelector('.btn').addEventListener('click', function(){
+        getDataCountr(inpuText.value);
+        inpuText.value = '';
+    })
